1e1913635fac58f469b3086657b1da057b318354
Suman Singh <sksin.132.official@gmail.com>
2026-08-20 21:13:38 +0545

    first api demo app

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+text_post = {1: {"title": "New Post", "content": "This is a new post."},
+             2: {"title": "Another Post", "content": "This is another post."},
+             3: {"title": "Yet Another Post", "content": "This is yet another post."},
+             4: {"title": "Final Post", "content": "This is the final post."},
+             5: {"title": "Last Post", "content": "This is the last post."},
+             6:{"title": "Extra Post", "content": "This is an extra post."},
+             7:{"title": "Bonus Post", "content": "This is a bonus post."},
+             8:{"title": "Additional Post", "content": "This is an additional post."},
+             9:{"title": "More Post", "content": "This is more post."},
+             10:{"title": "Even More Post", "content": "This is even more post."}
+}
+
+@app.get("/posts")
+def get_all_posts():
+    return text_post
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    if id not in text_post:
+        raise HTTPException(status_code=404, detail="Post not found")   
+    return text_post[id]
